d5d079c589bf5610c5c8f22d9ce5615a15a53e9e
RAVI PRATAP SINGH <ravipratapsingh3628@gmail.com>
2023-10-13 13:21:23 +0530

    use case

--- day_1_array/HOF/higheroOrderFUN.js
+++ day_1_array/HOF/higheroOrderFUN.js
@@ -6,11 +6,11 @@
 
 // Assigning a function to a variable 
 
-const foo = () => {
-    console.log("foobar")
-}
+// const foo = () => {
+//     console.log("foobar")
+// }
 
-foo()// Invoke it using the variable
+// foo()// Invoke it using the variable
 
 //We assigned an Anonymous Function in a Variable, then we used that variable to invoke the function by adding parentheses () at the end.
 
@@ -22,15 +22,15 @@ foo()// Invoke it using the variable
 
 // Passing a function as an argument
 
-function sayHello() {  // callback function 
-    return "hello"
-}
+// function sayHello() {  // callback function 
+//     return "hello"
+// }
 
-function greeting(hellmessage, name){
-    console.log(hellmessage() + name)
-}
+// function greeting(hellmessage, name){
+//     console.log(hellmessage() + name)
+// }
 
-greeting(sayHello, "javascript");
+// greeting(sayHello, "javascript");
 
 //We are passing our sayHello() function as an argument to the greeting() function, this explains how we are treating the function as a value.
 
@@ -44,12 +44,88 @@ greeting(sayHello, "javascript");
 //     };
 //   }
 
-function sayHe() {
-    return sayHello();
-  }
+// function sayHe() {
+//     return sayHello();
+//   }
 
 
   //In this example, we are returning a function from another function - We can return a function because functions in JavaScript are treated as values.
 
 
   ///Note: A function that returns a function or takes other functions as arguments is called a higher-order function.
+
+
+
+  // now let see the use case 
+
+  //1Q  now from a given array you have to skim out number values boolean values string values and to do that you have to create a function 
+
+//   let arr = ["ravi" , 53 , "pandu" , true , 98 , "gandu"]
+
+
+
+
+// function getString(arr){
+//     let res = []
+//     for(let item of arr){
+//         if(typeof(item)==="string"){
+
+//             res.push(item)
+
+//         }
+        
+//     }
+//     return res
+// }
+
+// console.log(getString(arr))
+
+// simlarly we have to do it for numbers and boolean 
+
+// function getNumbers(arr){
+//     let res = []
+//      for(let item of arr){
+//         if(typeof(item)=== 'number'){
+//             res.push(item)
+//         }
+//      }
+//       return res
+// }
+
+// console.log(getNumbers(arr))
+
+
+// function getBoolean(arr){
+//     let res = []
+
+//     for(let item of arr){
+//         if(typeof(item) ==="boolean"){
+//             res.push(item)
+//         }
+//     }
+//     return res
+// }
+
+// console.log(getBoolean(arr))
+
+// so writing this much code is a way to more   we can reduce it using hof 
+
+function getString(item){
+     return typeof(item) === 'string';
+}
+
+
+function get( arr , fn){
+    let res = [];
+
+    for(let item of arr){
+        if(fn(item)){
+            res.push(item)
+        }
+    }
+    return res
+}
+
+let arr = ["ravi" , 53 , "pandu" , true , 98 , "gandu"]
+
+get(arr, getString)
